d8237b96e2d79fb691ed96d61fa2e5ff9feb748f
Mark Nadal <mark@accelsor.com>
2017-03-13 19:08:29 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,23 +5,21 @@ var http = require('http');
 var port = process.env.PORT || 8080;
 var fs = require('fs');
 
-// Saves all data to `data.json`.
-var gun = Gun({
-	file: 'data.json',
-});
-
 // Listens on /gun.js route.
-var server = http.Server(gun.wsp.server);
+var server = http.Server();
 
 // Serves up /index.html
 server.on('request', function (req, res) {
+	if(Gun.serve(req, res)){ return }
 	if (req.url === '/' || req.url === '/index.html') {
 		fs.createReadStream('index.html').pipe(res);
 	}
 });
 
-// Handles real-time requests and updates.
-gun.wsp(server);
+var gun = Gun({
+	file: 'data.json', // Saves all data to `data.json`.
+	web: server // Handles real-time requests and updates.
+});
 
 server.listen(port, function () {
 	console.log('\nApp listening on port', port);
